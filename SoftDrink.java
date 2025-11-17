0f3f9488bf4562c278481648d4275fb00058a2fd
William9788 <hewohui9788@icloud.com>
2025-11-17 09:25:17 +0000

    push

--- SoftDrink.java
+++ SoftDrink.java
@@ -1,3 +1,18 @@
-public class SoftDrink {
-    
+public class SoftDrink extends Drink {
+    private final boolean fizzy;
+
+    public SoftDrink(double value, String flavour, boolean fizzy) {
+        super(value, flavour);
+        this.fizzy = fizzy;
+    }
+
+    public boolean isFizzy() {
+        return fizzy;
+    }
+
+    @Override
+    public double calcPrice() {
+        double base = getValue();
+        return fizzy ? base + 0.5 : base;
+    }
 }
