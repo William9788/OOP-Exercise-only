a9a6176fc68419bb7ff6852b110cbd7270405bd3
William9788 <hewohui9788@icloud.com>
2025-11-18 17:05:41 +0000

    update

--- Animal.java
+++ Animal.java
@@ -28,6 +28,15 @@ animalReference = new Animal();
 Dog wrongReference = (Dog) animalReference;
 wrongReference.fetch();//Not Executed
 }
+/*
+ * • Create an Animal reference that points to a Puppy object.
+• Then downcast that Animal reference back to a Dog, and then again to a
+Puppy.
+• Call one method from each level and see the output:
+• From Animal - call the makeSound() method.
+• From Dog - call the fetch() method.
+• From Puppy - call the play() method.
+ */
 class Puppy extends Dog{
 void play() {
 System.out.println ("Puppy is playing!");
